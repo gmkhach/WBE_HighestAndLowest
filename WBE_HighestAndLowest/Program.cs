ba173406780237e77f9be3163bfa9e966c3d26fa
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-26 12:32:40 -0400

    Add project files.

---
+++ WBE_HighestAndLowest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBE_HighestAndLowest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
